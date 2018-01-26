b686c8363a7e49735365447b95a25d060f5274e3
guillermosoftware <guillermomendez.ing@gmail.com>
2018-01-26 16:39:56 -0500

    1
    inicial

---
+++ js/temperatura.js
@@ -0,0 +1,32 @@
+angular
+.module('iot', [])
+.controller('temperatura_controller', ['$scope','$http', temperatura_controller]);
+
+function temperatura_controller($scope,$http){
+
+    $scope.obtenerDatos = function(){
+        var url= "https://iotmmsbc08f0ed0.us1.hana.ondemand.com/com.sap.iotservices.mms/v1/api/http/app.svc/";
+        var user= "mi usuario";
+        var clave= "mi clave";
+        //Invocar serivicio web IOT metodo GET. Tabla Hana de temperatura
+        $http.get(url, {
+            headers: {
+            "Authorization":$scope.make_base_auth(user, clave)
+            }
+          })
+          .then(function successCallback(res) {
+              debugger;
+            $scope.DataTemperature = res.data.d.results;
+          }, function errorCallback(res){
+              console.log("Error al invocar servicio SCP");
+          });
+        
+    },
+    //Funcion que permite cifrar la seguridad al invocar el servicio web
+    $scope.make_base_auth = function(user, password) {
+        var tok = user + ':' + password;
+        var hash = btoa(tok);
+        return 'Basic ' + hash;
+    }
+
+};
